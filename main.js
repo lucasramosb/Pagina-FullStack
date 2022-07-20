2488557fa3fe23ec7c86d28c4d28547b954e33a6
Lucas Ramos <81596809+lucasramosb@users.noreply.github.com>
2022-07-20 20:34:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+function cadastrar (nome, email, nascimento, telefone){
+    // let tb = document.getElementById("tblCadastro");
+    // let qtdRow = tb.length;
+    // let row = tb.InsertRow(qtdRow);
+
+    // let cellCodigo = row.InsertCell(0);
+    // let cellNome = row.InsertCell(1);
+    // let cellEmail = row.InsertCell(2);
+    // let cellNascimento = row.InsertCell(3);
+    // let celltelefone = row.InsertCell(4);
+
+    // cellCodigo.innerHTML = qtdRow;
+    // cellNome.innerHTML = nome;
+    // cellEmail.innerHTML = email;
+    // cellNascimento.innerHTML = nascimento;
+    // celltelefone.innerHTML = telefone;
+
+    // console.log(qtdRow)
+    // console.log(table)
+
+    
+
+    console.log(nome)
+
+}
